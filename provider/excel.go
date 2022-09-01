dc6574590ab9e572b8fc2373a2bf4496f78bb303
3BOL <g.winterholler3@gmail.com>
2022-09-01 11:58:33 +0200

    Excel Export Funktion geschrieben

--- provider/excel.go
+++ provider/excel.go
@@ -1,10 +1,16 @@
 package provider
 
+import (
+	"fmt"
+
+	"github.com/xuri/excelize/v2"
+)
+
 type excelProvider struct {
 }
 
 func (e *excelProvider) Name() string {
-	return ""
+	return "excel"
 }
 
 func (e *excelProvider) ValidateLogin(username string, password string) error {
@@ -16,7 +22,32 @@ func (e *excelProvider) NeedsLogin() bool {
 }
 
 func (e *excelProvider) Export(report *Report, startDate string, templateFile string, outputDir string) error {
-	return nil
+	outputFile := outputDir + "\\" + startDate + ".xlsx"
+	content := ""
+	f, err := excelize.OpenFile(templateFile)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	defer func() {
+		// Close the spreadsheet.
+		if err := f.Close(); err != nil {
+			fmt.Println(err)
+		}
+	}()
+
+	for _, v := range report.Entries {
+		content += "- " + v.Text + " \n"
+	}
+
+	f.SetCellValue(f.GetSheetName(0), "A42", content)
+	fmt.Println("Excel Export succeed!")
+	if err := f.SaveAs(outputFile); err != nil {
+		fmt.Println(err)
+		return err
+	}
+
+	return err
 }
 
 func NewExcelProvider() ExportProvider {
