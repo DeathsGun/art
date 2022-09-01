92a359ee90cb3d8a877169de4a390dae6d9839e2
PKMNPlatin <jschmidt@dh-software.de>
2022-09-01 11:57:59 +0200

    Added GetTimetable function @UntisAPI

--- api/untis.go
+++ api/untis.go
@@ -7,16 +7,29 @@ import (
 
 type Untis struct {
 	client        *rpc.Client
-	loginResponse *LoginResponse
+	loginResponse LoginResponse
 }
 
 func (u *Untis) Login(username string, password string) error {
-	err := u.client.Call("authenticate", LoginRequest{Username: username, Password: password, Client: uuid.NewString()}, u.loginResponse)
+	loginResponse := &LoginResponse{}
+	err := u.client.Call("authenticate", LoginRequest{Username: username, Password: password, Client: uuid.NewString()}, loginResponse)
+	if err == nil {
+		u.loginResponse = *loginResponse
+	}
 	return err
 }
 
 func (u *Untis) Logout() error {
 	return u.client.Call("logout", nil, nil)
+}
+
+func (u *Untis) GetTimetable(request *TimetableRequest) (*[]TimetablePeriod, error) {
+	result := &[]TimetablePeriod{}
+	err := u.client.Call("getTimetable", request, result)
+	if err != nil {
+		return nil, err
+	}
+	return result, nil
 }
 
 func NewUntisAPI(schoolName string) (*Untis, error) {
@@ -28,16 +41,59 @@ func NewUntisAPI(schoolName string) (*Untis, error) {
 }
 
 type Person int
+type LessonType string
+type PeriodState string
 
 const (
 	Teacher  Person = 2
 	Students Person = 5
 )
 
+const (
+	Lesson           LessonType = "ls"
+	OfficeHour       LessonType = "oh"
+	Standby          LessonType = "sb"
+	BreakSupervision LessonType = "bs"
+	Examination      LessonType = "ex"
+)
+
+const (
+	Default   PeriodState = ""
+	Cancelled PeriodState = "cancelled"
+	Irregular PeriodState = "irregular"
+)
+
+type TimetablePeriod struct {
+	Id           int              `json:"id"`
+	Date         int              `json:"date"`
+	StartDate    int              `json:"startDate"`
+	EndDate      int              `json:"endDate"`
+	Class        []PeriodListType `json:"kl"`
+	Teachers     []PeriodListType `json:"te"`
+	Subjects     []PeriodListType `json:"su"`
+	Rooms        []PeriodListType `json:"ro"`
+	Type         LessonType       `json:"lstype"`
+	State        PeriodState      `json:"code"`
+	Text         string           `json:"lstext"`
+	Flags        string           `json:"statflags"`
+	ActivityType string           `json:"activityType"`
+}
+
+type PeriodListType struct {
+	Id int `json:"id"`
+}
+
 type LoginResponse struct {
 	SessionId  string `json:"sessionId"`
 	PersonType Person `json:"personType"`
 	PersonId   int    `json:"personId"`
+}
+
+type TimetableRequest struct {
+	Id        int `json:"id"`
+	Type      int `json:"type"`
+	StartDate int `json:"startDate"`
+	EndDate   int `json:"endDate"`
 }
 
 type LoginRequest struct {
--- api/untis_test.go
+++ api/untis_test.go
@@ -10,11 +10,28 @@ func TestLogin(t *testing.T) {
 	untis, err := NewUntisAPI("bk-ahaus")
 	err = untis.Login("schm132074", os.Getenv("UNTIS_PW"))
 	if err != nil {
-		t.Fatal(err)
+		panic(err)
 	}
-	fmt.Println(untis.loginResponse.SessionId)
 	err = untis.Logout()
 	if err != nil {
 		t.Fatal(err)
 	}
+}
+
+func TestTimetable(t *testing.T) {
+	untis, err := NewUntisAPI("bk-ahaus")
+	err = untis.Login("schm132074", os.Getenv("UNTIS_PW"))
+	if err != nil {
+		t.Fatal(err)
+	}
+	timetable, err := untis.GetTimetable(&TimetableRequest{
+		Id:        untis.loginResponse.PersonId,
+		Type:      int(untis.loginResponse.PersonType),
+		StartDate: 20220821,
+		EndDate:   20220901,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	fmt.Printf("%+v", timetable)
 }
